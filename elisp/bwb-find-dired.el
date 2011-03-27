540aa48ea0a5982d843f040fbe4c46eac9d9e125
Brandon Barry <brandon.w.barry@gmail.com>
2011-03-27 11:25:56 -0400

    Fix an error in bwb-find-dired; update bookmark files.

--- elisp/bwb-find-dired.el
+++ elisp/bwb-find-dired.el
@@ -1,4 +1,7 @@
 ;;;; Configure `find-dired'.
+(require 'find-dired)
 
 ;;; http://www.masteringemacs.org/articles/2011/03/25/working-multiple-files-dired/
 (setq find-ls-option '("-print0 | xargs -0 ls -ld" . "-ld"))
+
+(provide 'bwb-find-dired)
