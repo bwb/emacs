7a5c3cfe696f697fa8a36467b7126de5b78a8621
Brandon Barry <brandon.w.barry@gmail.com>
2011-04-02 17:38:27 -0400

    Commit fewer terminal errors; tweak bookmarks.

--- elisp/bwb-bookmark+.el
+++ elisp/bwb-bookmark+.el
@@ -1,7 +1,8 @@
 ;;; Configure the `bookmark' enhancement `bookmark+'.
 (require 'bookmark+)
 
-(setq bookmark-default-file "~/emacs/bookmark/main.bmk")
+(setq bookmark-default-file "~/emacs/bookmark/main.bmk"
+      bookmark-save-flag 1)
 (global-set-key (kbd "C-x m") 'bookmark-map)
 ;; TODO Add to `bmkp-default-handler-associations'
 
--- elisp/bwb-multi-term.el
+++ elisp/bwb-multi-term.el
@@ -2,11 +2,17 @@
 (require 'multi-term)
 
 (setq multi-term-program "/bin/bash")
+
 ;; Rebind word kill commands.  Otherwise WYSI*NOT*WYG!
 (add-to-list 'term-bind-key-alist
              '("M-DEL" . term-send-backward-kill-word))
 (add-to-list 'term-bind-key-alist
+             '("C-DEL" . term-send-backward-kill-word))
+(add-to-list 'term-bind-key-alist
              '("M-d" . term-send-forward-kill-word))
+
+(define-key term-raw-map (kbd "C-c C-j") 'term-line-mode)
+
 (add-hook 'term-mode-hook
           (lambda ()
             (setq show-trailing-whitespace nil)))
