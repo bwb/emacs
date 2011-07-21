2476fe854c54f3c72e33163328475c604ac9ac20
Brandon Barry <brandon.w.barry@gmail.com>
2011-07-21 10:57:50 -0400

    Improve rst-mode-map.

--- elisp/bwb-rst.el
+++ elisp/bwb-rst.el
@@ -8,6 +8,12 @@
             (setq rst-toc-insert-max-level 2)))
 (add-to-list 'auto-mode-alist '("\\.rst$" . rst-mode))
 
+;; By default `rst-mode' uses the `beginning-of-defun' binding for
+;; `rst-forward-section', and the `end-of-defun' binding for
+;; `rst-backward-section'.  This feels backwards.  Swap the bindings.
+(define-key rst-mode-map (kbd "C-M-a") 'rst-backward-section)
+(define-key rst-mode-map (kbd "C-M-e") 'rst-forward-section)
+
 ;;; TODO Write a command (or snippet) to generate :ROLE:`INTERPRETED
 ;;; TEXT <BLAH>`
 
