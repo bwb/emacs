6f77c1b7c5065b63a00ada776610889fbfb17f10
Brandon Barry <brandon.w.barry@gmail.com>
2012-02-13 14:34:02 -0500

    Tweak ido-mode. Use apache-mode.

--- elisp/bwb-ido.el
+++ elisp/bwb-ido.el
@@ -10,5 +10,6 @@
   (setq ido-enable-flex-matching t
         ido-everywhere t
         ido-create-new-buffer 'always
+        ido-max-directory-size 100000
         ido-use-filename-at-point 'guess
         ido-use-virtual-buffers t))
