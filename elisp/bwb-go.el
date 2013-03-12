4ad42e228ca7b6f16bfb019ade5fd7aeecb5c23d
Brandon Barry <brandon.w.barry@gmail.com>
2013-03-11 23:47:29 -0400

    Improve go-mode.

--- elisp/bwb-go.el
+++ elisp/bwb-go.el
@@ -1,7 +1,7 @@
 ;;; Configure `go-mode'.
 ;;;###autoload
+
 (progn
   (require 'go-mode-load)
-  ;; TODO enable gofmt and godoc
-  ;; (add-hook 'before-save-hook #'gofmt-before-save)
-  )
+  (require 'go-autocomplete)
+  (add-hook 'before-save-hook 'gofmt-before-save))
