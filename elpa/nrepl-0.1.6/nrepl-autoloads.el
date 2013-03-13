6b5c82f474d662a7c36a99c59d4014984ba5a06d
Brandon Barry <brandon.w.barry@gmail.com>
2013-03-13 11:27:17 -0400

    Upgrade packages. Upgrade js2-mode.

--- elpa/nrepl-0.1.6/nrepl-autoloads.el
+++ elpa/nrepl-0.1.6/nrepl-autoloads.el
@@ -5,26 +5,30 @@
 
 ;;;### (autoloads (nrepl nrepl-jack-in nrepl-disable-on-existing-clojure-buffers
 ;;;;;;  nrepl-enable-on-existing-clojure-buffers nrepl-interaction-mode)
-;;;;;;  "nrepl" "nrepl.el" (20732 53149))
+;;;;;;  "nrepl" "nrepl.el" (20800 39075 0 0))
 ;;; Generated autoloads from nrepl.el
 
 (autoload 'nrepl-interaction-mode "nrepl" "\
 Minor mode for nrepl interaction from a Clojure buffer.
 
+\\{nrepl-interaction-mode-map}
+
 \(fn &optional ARG)" t nil)
 
 (autoload 'nrepl-enable-on-existing-clojure-buffers "nrepl" "\
-
+Enable `nrepl-interaction-mode' on existing Clojure buffers.
 
 \(fn)" t nil)
 
 (autoload 'nrepl-disable-on-existing-clojure-buffers "nrepl" "\
-
+Disable `nrepl-interaction-mode' on existing Clojure buffers.
 
 \(fn)" t nil)
 
 (autoload 'nrepl-jack-in "nrepl" "\
-
+Start a nREPL server for the current project and connect to it.
+If PROMPT-PROJECT is t, then prompt for the project for which to
+start the server.
 
 \(fn &optional PROMPT-PROJECT)" t nil)
 
@@ -37,7 +41,7 @@ Minor mode for nrepl interaction from a Clojure buffer.
 
 ;;;***
 
-;;;### (autoloads nil nil ("nrepl-pkg.el") (20732 53149 459010))
+;;;### (autoloads nil nil ("nrepl-pkg.el") (20800 39075 946190 779000))
 
 ;;;***
 
