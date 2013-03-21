e937b073df69e7a2c34a835a9c24d465aca4d605
Brandon Barry <brandon.w.barry@gmail.com>
2013-03-20 23:06:39 -0400

    Work with go-mode.

--- elisp/bwb-go.el
+++ elisp/bwb-go.el
@@ -1,7 +1,16 @@
 ;;; Configure `go-mode'.
-;;;###autoload
+;;; http://dominik.honnef.co/posts/2013/03/writing_go_in_emacs/
+;;;
+;;; TODO update Emacs' environment using "go env" in current project.
+;;; TODO push and pop godef-jump locations.
 
+;;;###autoload
+(defun bwb-go-bind-keys ()
+    (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports))
+
+;;;###autoload
 (progn
   (require 'go-mode-load)
   (require 'go-autocomplete)
+  (add-hook 'go-mode-hook 'bwb-go-bind-keys)
   (add-hook 'before-save-hook 'gofmt-before-save))
