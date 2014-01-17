3c2ed5a7d57d9ed7c640618f6e1a541a4571548e
Brandon Barry <brandon.w.barry@gmail.com>
2014-01-17 15:33:18 -0500

    Upgrade packages.

--- packages/ace-jump-buffer-20130627.2104/ace-jump-buffer-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; ace-jump-buffer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (ace-jump-buffer) "ace-jump-buffer" "ace-jump-buffer.el"
-;;;;;;  (21021 26390 0 0))
-;;; Generated autoloads from ace-jump-buffer.el
-
-(autoload 'ace-jump-buffer "ace-jump-buffer" "\
-Quickly hop between buffers with `ace-jump-mode'
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("ace-jump-buffer-pkg.el") (21021 26390
-;;;;;;  559869 0))
-
-;;;***
-
-(provide 'ace-jump-buffer-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ace-jump-buffer-autoloads.el ends here
--- packages/writegood-mode-20130406.2316/writegood-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "writegood-mode" "20130406.2316" "Polish up poor writing on the fly [github]" (quote nil))
