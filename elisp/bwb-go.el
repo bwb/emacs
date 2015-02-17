3cbba8acfe02c329cdd80315f1c1faeb8e0797bb
Brandon Barry <brandon.w.barry@gmail.com>
2015-02-16 22:30:48 -0500

    Simplify the insertion of ":=" and "<-". The insert functions would probably work better as snippets.

--- elisp/bwb-go.el
+++ elisp/bwb-go.el
@@ -16,11 +16,26 @@
   (newline-and-indent))
 
 ;;;###autoload
+(defun bwb-go-insert-colon-equals ()
+  (interactive)
+  (insert " := ")
+  (indent-according-to-mode))
+
+;;;###autoload
+(defun bwb-go-insert-channel-operator ()
+  (interactive)
+  (insert " <- ")
+  (indent-according-to-mode))
+
+;;;###autoload
 (defun bwb-go-bind-keys ()
   (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
   (local-set-key (kbd "C-<return>") 'bwb-go-end-of-line-newline-and-indent)
   (local-set-key (kbd "C-S-<return>") 'bwb-go-insert-expanded-braces)
-  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports))
+  (local-set-key (kbd "C-'") 'bwb-go-insert-colon-equals)
+  (local-set-key (kbd "C-,") 'bwb-go-insert-channel-operator)
+  (local-set-key (kbd "C-<") 'bwb-go-insert-channel-operator))
 
 ;;;###autoload
 (progn
