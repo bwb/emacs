e24f1d4912877eb3fa2185709437383881f98827
Brandon Barry <brandon.w.barry@gmail.com>
2017-09-18 16:12:28 -0400

    Mitigate "content-type: text/enriched" vuln

--- init.el
+++ init.el
@@ -23,6 +23,12 @@
 ;; Don't sleep Emacs accidentally.
 (global-set-key (kbd "C-z") nil)
 
+;; Mitigate Bug#28350 (security) in Emacs 25.2 and earlier.
+;; http://seclists.org/oss-sec/2017/q3/422
+(eval-after-load "enriched"
+  '(defun enriched-decode-display-prop (start end &optional param)
+     (list start end)))
+
 ;;; `~/emacs/packages'
 ;;; http://www.emacswiki.org/emacs/ELPA
 ;;; http://melpa.org/
